4296de4278babbd7456c1171c5655f0b974d4628
sabin770 <sabintapkota@gmial.com>
2026-09-06 21:08:18 +0545

    Packages and OOP Integration

---
+++ Practical/src/PackageDemo.java
@@ -0,0 +1,8 @@
+import mypackage.Student;
+
+public class PackageDemo {
+    public static void main(String[] args) {
+        Student s = new Student(101, "Savsi", "BCA");
+        s.display();
+    }
+}
