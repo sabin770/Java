0167af2c3a93f9afde382a11c27ac04169d4683c
sabin770 <sabintapkota@gmial.com>
2026-09-06 22:13:16 +0545

    Packages and OOP Integration

---
+++ Practical/src/MainApp.java
@@ -0,0 +1,18 @@
+import mypackage.Studentt;
+
+public class MainApp {
+    public static void main(String[] args) {
+        // Creating objects using constructor
+        Studentt s1 = new Studentt("Savs", 20);
+        Studentt s2 = new Studentt("Bavs", 22);
+        Studentt s3 = new Studentt("Cavs", 21);
+
+        // Display each student
+        s1.display();
+        s2.display();
+        s3.display();
+
+        // Using static method
+        System.out.println("Total Students: " + Studentt.getTotalStudentts());
+    }
+}
