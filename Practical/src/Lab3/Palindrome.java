97de5c41b03da7842a22b9b777f4808c9226214c
sabin770 <sabintapkota@gmail.com>
2026-09-13 10:18:59 +0545

    Lab 3: Arrays, Strings and StringBuffer (search, sort, matrix, jagged array, palindrome, string methods, StringBuffer)

---
+++ Practical/src/Lab3/Palindrome.java
@@ -0,0 +1,25 @@
+// Question 7: Write a Java program to check whether a given String is a palindrome.
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        String reversed = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+
+        if (str.equals(reversed)) {
+            System.out.println(str + " is a Palindrome");
+        } else {
+            System.out.println(str + " is NOT a Palindrome");
+        }
+
+        sc.close();
+    }
+}
